b192cec2b8f597baf41672e9dea3bb6af92d4b36
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-21 16:20:12 +0530

    Update ronis3.c

--- ronis3.c
+++ ronis3.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char s[30];
+  gets(s);
+  int i;      
+  int num = 0;
+  for(i =0;i<strlen(s);i++) {
+  if(s[i] == ' ')
+  {
+num++;
+ }
+ }
+ printf("%d\n", num);
+ getch();
+ return 0;
+ } 
 
