0c45e5b1e8ad0fa71930590dc91809cb9d63ec17
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-21 16:21:27 +0530

    Create ronis4.c

---
+++ ronis4.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+ char s[100];
+  gets(s);
+  int i;      
+  int num = 0;
+  for(i =0;i<strlen(s);i++) {
+  if(s[i] =='.' || s[i]==' ')
+  {
+num++;
+ }
+ }
+ printf("%d\n",num);
+ getch ();
+ return 0;
+ }
